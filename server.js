a5f45edfe570a22ec817e81eec2793424b42214e
Rahul K P <rahulkp01aug@gmail.com>
2022-07-22 14:38:14 +0530

    Added a get function

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8080;
+
+app.get('/', (req , res)=> {
+	res.sendFile('index.html');
+});
 
 app.listen(port, () => {
 	console.log('Listening on port' + port);
